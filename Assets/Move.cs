80df3bf2275dcbd4ef4bfb932ae6872fe1aa2ccb
Lauri Rasi <lauri.rasi@student.tut.fi>
2018-01-26 20:37:46 +0200

    Lisää alustava pressaobjekti ja sille yksinkertainen liikkumis-scripta

---
+++ Assets/Move.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    Vector2 location;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        location.x = location.x + (float)0.1;
+        location.y = location.y + (float)0.1;
+        MoveTo();
+	}
+
+    void MoveTo ()
+    {
+        transform.Translate(location);
+    }
+}
