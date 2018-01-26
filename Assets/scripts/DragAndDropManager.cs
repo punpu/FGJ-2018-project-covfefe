a2644354cf4bbc88b209f2d6490172caaf8ffc11
Tuomas Kunnamo <t.a.kunnamo@gmail.com>
2018-01-26 22:30:39 +0200

    Added simple drag and drop

---
+++ Assets/scripts/DragAndDropManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDropManager : MonoBehaviour
+{
+    private bool draggingItem = false;
+    private GameObject draggedObject;
+    private Vector2 touchOffset;
+    private Vector3 draggedObjectScale;
+
+    void Update()
+    {
+        if (HasInput)
+        {
+            DragOrPickUp();
+        }
+        else
+        {
+            if (draggingItem)
+            {
+                DropItem();
+            }
+        }
+    }
+
+    Vector2 CurrentTouchPosition
+    {
+        get
+        {
+            Vector2 inputPos;
+            inputPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            return inputPos;
+        }
+    }
+
+    private void DragOrPickUp()
+    {
+        var inputPosition = CurrentTouchPosition;
+
+        if (draggingItem)
+        {
+            draggedObject.transform.position = inputPosition + touchOffset;
+        }
+        else
+        {
+            RaycastHit2D[] touches = Physics2D.RaycastAll(inputPosition, inputPosition, 0.5f);
+            if (touches.Length > 0)
+            {
+                var hit = touches[0];
+
+                if (hit.transform != null && hit.transform.gameObject.CompareTag("InventoryItem"))
+                {
+                    draggingItem = true;
+                    draggedObject = hit.transform.gameObject;
+                    draggedObjectScale = draggedObject.transform.localScale;
+
+                    touchOffset = (Vector2)hit.transform.position - inputPosition;
+                    draggedObject.transform.localScale = new Vector3(draggedObjectScale.x * 1.2f, draggedObjectScale.y * 1.2f);
+                }
+            }
+        }
+    }
+
+    private bool HasInput
+    {
+        get
+        {
+            // returns true if either the mouse button is down or at least one touch is felt on the screen
+            return Input.GetMouseButton(0);
+        }
+    }
+
+    void DropItem()
+    {
+        draggingItem = false;
+        draggedObject.transform.localScale = draggedObjectScale;
+        draggedObject = null;
+    }
+}
