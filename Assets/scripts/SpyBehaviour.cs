132a17ca527e97fc8fc726db7dc62ca015fa4243
Tuomas Kunnamo <t.a.kunnamo@gmail.com>
2018-01-27 22:58:20 +0200

    More items clickable

--- Assets/scripts/SpyBehaviour.cs
+++ Assets/scripts/SpyBehaviour.cs
@@ -9,6 +9,9 @@ public class SpyBehaviour : MonoBehaviour {
     public float periodMax = 1f;
     public float moveSpeed = 2f;
 
+    public int health = 0;
+    public int healthMax = 5;
+
     public bool isActive;
     public GameObject president;
     public Vector2 activePosition;
@@ -19,6 +22,7 @@ public class SpyBehaviour : MonoBehaviour {
     {
         gameObject.GetComponent<TransmitterBehavior>().isTransmitting = true;
         isActive = true;
+        health = healthMax;
     }
 
     public void Deactivate()
@@ -63,6 +67,19 @@ public class SpyBehaviour : MonoBehaviour {
         {
             float step = moveSpeed * Time.deltaTime;
             transform.position = Vector2.MoveTowards(transform.position, hiddenPosition, step);
+        }
+    }
+
+
+    void OnMouseDown()
+    {
+        if (isActive)
+        {
+            health--;
+            if (health <= 0)
+            {
+                Deactivate();
+            }
         }
     }
 }
