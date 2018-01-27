3b1afa5c2d12821c4a83e2c242c8ba8a4d1675aa
Minna Jarsma <minna.jarsma@gmail.com>
2018-01-27 12:47:10 +0200

    Pressan liikkuminen lähintä lähetystä kohti

--- Assets/scripts/PresidentBehavior.cs
+++ Assets/scripts/PresidentBehavior.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PresidentBehavior : MonoBehaviour {
@@ -10,25 +11,52 @@ public class PresidentBehavior : MonoBehaviour {
 	public float minTime = 0.5f;
 	public float maxTime = 1.5f;
 	public float moveSpeed = 1;
+    public string transmitterTag = "Transmitter";
 
 	private float tChange = 0;
     private float randomX;
     private float randomY;
+    private GameObject[] transmitters;
 
 	// Use this for initialization
 	void Start () {
-		
+        transmitters = GameObject.FindGameObjectsWithTag(transmitterTag);
+        Debug.Log(transmitters);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		// Random direction at random intervals
-        if (Time.time >= tChange) {
-			randomX = Random.Range (minX, maxY);
-			randomY = Random.Range (minY, maxY);
-            tChange = Time.time + Random.Range (minTime, maxTime);
+        GameObject activeTransmitter = GetClosestActiveTransmitter();
+        if (activeTransmitter != null) {
+            float step = moveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, activeTransmitter.transform.position, step);
         }
-        float step = moveSpeed * Time.deltaTime;
-        transform.position = Vector3.MoveTowards(transform.position, new Vector3(randomX, randomY, 0), step);
+        else {
+            // Random direction at random intervals
+            if (Time.time >= tChange)
+            {
+                randomX = Random.Range(minX, maxY);
+                randomY = Random.Range(minY, maxY);
+                tChange = Time.time + Random.Range(minTime, maxTime);
+            }
+            float step = moveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, new Vector2(randomX, randomY), step);
+        }
+    }
+
+    GameObject GetClosestActiveTransmitter () {
+        List<GameObject> activeTransmitters = new List<GameObject>();
+        if (!transmitters.Any()) {
+            return null;
+        }
+        foreach (var obj in transmitters) {
+            if (obj.GetComponent<TransmitterBehavior>().isTransmitting) {
+                activeTransmitters.Add(obj);
+            }
+        }
+        activeTransmitters = activeTransmitters.OrderBy(
+            x => Vector2.Distance(transform.position, x.transform.position)
+           ).ToList();
+        return activeTransmitters.Any() ? activeTransmitters[0] : null;
     }
 }
