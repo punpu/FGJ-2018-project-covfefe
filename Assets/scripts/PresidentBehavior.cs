565621bed2ea6da45197989b394fd8dbf495530a
Minna Jarsma <minna.jarsma@gmail.com>
2018-01-27 22:53:48 +0200

    Fiksaa pressan käytös

--- Assets/scripts/PresidentBehavior.cs
+++ Assets/scripts/PresidentBehavior.cs
@@ -14,11 +14,11 @@ public class PresidentBehavior : MonoBehaviour {
     public string transmitterTag = "Transmitter";
     public bool tinfoilHatActive = false;
 
-	private float tChange = 0;
-    private float randomX;
-    private float randomY;
+    public float randomX;
+    public float randomY;
     private GameObject[] transmitters;
     private Animator animator;
+    private Vector3 randomTarget;
 
     private Transform tinfoilHat;
 
@@ -30,6 +30,9 @@ public class PresidentBehavior : MonoBehaviour {
         animator = GetComponent<Animator>();
         Debug.Log(transmitters);
         tinfoilHat = transform.Find("tinfoilHat");
+        randomX = Random.Range(minX, maxX);
+        randomY = Random.Range(minY, maxY);
+        randomTarget = new Vector3(randomX, randomY, 0);
     }
 	
 	// Update is called once per frame
@@ -42,17 +45,15 @@ public class PresidentBehavior : MonoBehaviour {
             transform.position = Vector2.MoveTowards(transform.position, activeTransmitter.transform.position, step);
         }
         else {
-            // Random direction at random intervals
-            if (Time.time >= tChange)
-            {
-                randomX = Random.Range(minX, maxY);
+            if (transform.position == randomTarget) {
+                randomX = Random.Range(minX, maxX);
                 randomY = Random.Range(minY, maxY);
-                tChange = Time.time + Random.Range(minTime, maxTime);
+                randomTarget = new Vector3(randomX, randomY, 0);
             }
             float step = moveSpeed * Time.deltaTime;
             bool isMovingLeft = transform.position.x <= randomX;
             animator.SetBool("isMovingLeft", isMovingLeft);
-            transform.position = Vector2.MoveTowards(transform.position, new Vector2(randomX, randomY), step);
+            transform.position = Vector2.MoveTowards(transform.position, randomTarget, step);
         }
     }
 
