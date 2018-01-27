018d6c3cd3481df3f860e6754d2e4e5054c46967
Minna Jarsma <minna.jarsma@gmail.com>
2018-01-27 22:08:10 +0200

    Huono loppu scene

---
+++ Assets/scripts/FingerBehavior.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class FingerBehavior : MonoBehaviour {
+    public float moveSpeed = 1f;
+
+    void Update () {
+        float step = moveSpeed * Time.deltaTime;
+        transform.position = Vector2.MoveTowards(transform.position, new Vector2(0, 3), step);
+	}
+}
