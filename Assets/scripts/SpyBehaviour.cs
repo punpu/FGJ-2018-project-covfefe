3fc2d3060f6552fd0b99cec9055c5143b51891fd
Lauri Rasi <lauri.rasi@student.tut.fi>
2018-01-27 19:07:23 +0200

    spy behaviour and updated prefab

--- Assets/scripts/SpyBehaviour.cs
+++ Assets/scripts/SpyBehaviour.cs
@@ -4,10 +4,47 @@ using UnityEngine;
 
 public class SpyBehaviour : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public bool isTransmitting;
+    public GameObject president;
+    public Vector2 activePosition;
+    public Vector2 hiddenPosition;
+    public float panicCount;
+
+    public void Activate()
+    {
+        Ascend();
+        isTransmitting = true;
+    }
+
+    public void Deactivate()
+    {
+        isTransmitting = false;
+        Descend();
+    }
+
+    public void Transmit()
+    {
+        president.SendMessage("ApplyPanic", panicCount);
+    }
+
+    // spy disappears
+    private void Descend()
+    {
+        transform.Translate(activePosition);
+    }
+
+    // A suspicious spy appears
+    private void Ascend()
+    {
+        transform.Translate(hiddenPosition);
+    }
+
+    // Use this for initialization
+    void Start () {
+        isTransmitting = false;
+        president = GameObject.FindGameObjectWithTag("president");
+        panicCount = 2;
+    }
 	
 	// Update is called once per frame
 	void Update () {
